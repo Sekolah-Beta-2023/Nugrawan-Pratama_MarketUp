d19960e43b1376450e9adace0d906d040749eb96
Nugrawan <126413672+Nugrawan@users.noreply.github.com>
2023-10-05 10:22:55 +0700

    add products

---
+++ pages/products/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <main id="content">
+    <h1 class="text-5xl font-bold text-center text-secondary p-5">ALL PRODUCTS</h1>
+
+    <CardContainer title="Men's Clothing">
+      <CardItem :data="menData"/>
+    </CardContainer>
+    
+    <CardContainer title="Jewelery" :primary="false">
+      <CardItem :data="jeweleryData" :is-clothes="false"/>
+    </CardContainer>
+    
+    <CardContainer title="Electronicts">
+      <CardItem :data="eletData" :is-clothes="false"/>
+    </CardContainer>
+
+    <CardContainer title="Women's Clothing" :primary="false">
+      <CardItem :data="womenData"/>
+    </CardContainer>
+
+  </main>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  // middleware: 'auth',
+  computed: {
+    ...mapState(['menData', 'womenData', 'jeweleryData', 'eletData']),
+  },
+  mounted() {
+    this.fetchMenProducts()
+    this.fetchWomenProducts()
+    this.fetchJeweleryProducts()
+    this.fetchElectronictsProducts()
+  },
+  methods: {
+    ...mapActions(['fetchMenProducts', 'fetchWomenProducts', 'fetchJeweleryProducts', 'fetchElectronictsProducts'])
+  }
+}
+</script>
